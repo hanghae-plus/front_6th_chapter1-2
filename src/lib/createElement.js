4772de4bfcd140dcc598972c878d716b4da2b8ee
yong <yong@thecodit.com>
2025-07-13 03:38:40 +0900

    feat: createElement basic test

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,72 @@
-import { addEvent } from "./eventManager";
+// import { addEvent } from "./eventManager";
+import { convert } from "../utils/converter";
+import { isNil } from "../utils/isNil";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  return basicCreateElement(vNode)
+    .merge(() => componentCreateElement(vNode))
+    .otherwise((val) => {
+      const $el = document.createElement(val.type);
+      updateAttributes($el, val.props);
+      if (val.children && val.children.length > 0) {
+        const $children = createElement(val.children);
+        $el.appendChild($children);
+      }
+      return $el;
+    })
+    .end();
+}
 
-function updateAttributes($el, props) {}
+const basicCreateElement = (vNode) => {
+  return convert(vNode, { earlyTermination: true })
+    .when((val) => isNil(val) || typeof val === "boolean")
+    .then(() => {
+      return document.createTextNode("");
+    })
+    .when((val) => typeof val === "string" || typeof val === "number")
+    .then((val) => {
+      return document.createTextNode(val.toString());
+    })
+    .when((val) => Array.isArray(val))
+    .then((val) => {
+      if (val.length === 1) {
+        return createElement(val[0]);
+      }
+
+      const $el = new DocumentFragment();
+      val.forEach((child) => {
+        const $child = createElement(child);
+        $el.appendChild($child);
+      });
+      return $el;
+    });
+};
+
+const isUnnormalizedComponent = (vNode) => {
+  return typeof vNode === "object" && typeof vNode.type === "function";
+};
+
+const componentCreateElement = (vNode) => {
+  return convert(vNode, { earlyTermination: true })
+    .when(isUnnormalizedComponent)
+    .error("Component should be normalized");
+};
+
+function updateAttributes($el, props) {
+  if (props) {
+    Object.entries(props).forEach(([key, value]) => {
+      $el.setAttribute(normalizeAttributeKey(key), value);
+    });
+  }
+}
+
+const normalizeAttributeKey = (key) => {
+  switch (key) {
+    case "className":
+      return "class";
+    case "htmlFor":
+      return "for";
+    default:
+      return key;
+  }
+};
--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,4 @@
+import { convert } from "../utils/converter";
 import { isNil } from "../utils/isNil";
 
 export function normalizeVNode(vNode) {
@@ -6,7 +7,7 @@ export function normalizeVNode(vNode) {
     .then("")
     .when((val) => typeof val === "string" || typeof val === "number")
     .then((val) => val.toString())
-    .when((val) => val && typeof val === "object" && typeof val.type === "function")
+    .when((val) => typeof val === "object" && typeof val.type === "function")
     .then((val) => {
       const propsWithChildren = {
         ...val.props,
@@ -16,7 +17,7 @@ export function normalizeVNode(vNode) {
 
       return normalizeVNode(result);
     })
-    .when((val) => val && typeof val === "object" && val.type && val.children)
+    .when((val) => typeof val === "object" && val.type && val.children)
     .then((val) => {
       const normalizedChildren = val.children.map((child) => normalizeVNode(child)).filter((child) => child !== ""); // falsy 값 제거
 
@@ -27,57 +28,4 @@ export function normalizeVNode(vNode) {
     })
     .otherwise((val) => val)
     .end();
-}
-
-function convert(input, { earlyTermination = true } = {}) {
-  return new Converter(input, { earlyTermination });
-}
-
-class Converter {
-  constructor(input, options = {}) {
-    this.value = input;
-    this.earlyTermination = options.earlyTermination ?? true;
-    this.hasTransformed = false;
-    this.pendingCondition = null;
-  }
-
-  when(condition) {
-    if (this.hasTransformed && this.earlyTermination) {
-      return this;
-    }
-
-    this.pendingCondition = condition;
-    return this;
-  }
-
-  then(transform) {
-    if (this.hasTransformed && this.earlyTermination) {
-      return this;
-    }
-
-    if (this.pendingCondition) {
-      const shouldApply =
-        typeof this.pendingCondition === "function" ? this.pendingCondition(this.value) : this.pendingCondition;
-
-      if (shouldApply) {
-        this.value = typeof transform === "function" ? transform(this.value) : transform;
-        this.hasTransformed = true;
-      }
-
-      this.pendingCondition = null;
-    }
-
-    return this;
-  }
-
-  otherwise(fallback) {
-    if (!this.hasTransformed) {
-      this.value = typeof fallback === "function" ? fallback(this.value) : fallback;
-    }
-    return this;
-  }
-
-  end() {
-    return this.value;
-  }
 }
