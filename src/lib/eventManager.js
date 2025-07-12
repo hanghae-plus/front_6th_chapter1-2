1edf513ff74c3d7c10444495677afd863336b642
YeongseoYoon <l112xpla@gmail.com>
2025-07-13 00:15:50 +0900

    feat: setupEventListeners 추가

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,50 @@
-export function setupEventListeners(root) {}
+const eventTypes = [
+  "click",
+  "dblclick",
+  "mousedown",
+  "mouseup",
+  "mousemove",
+  "keydown",
+  "keyup",
+  "input",
+  "change",
+  "focus",
+  "blur",
+];
 
-export function addEvent(element, eventType, handler) {}
+const eventMap = new WeakMap();
 
-export function removeEvent(element, eventType, handler) {}
+/**
+ * 컨테이너에 이벤트 리스너를 한 번만 등록합니다.
+ *
+ * @param {HTMLElement} container - 이벤트 리스너를 등록할 컨테이너 엘리먼트
+ */
+export function setupEventListeners(container) {
+  if (container.getAttribute("data-event-listeners") === "true") {
+    return;
+  }
+
+  eventTypes.forEach((eventType) => {
+    container.addEventListener(eventType, (e) => {
+      let target = e.target;
+      while (target && target !== container) {
+        const elementEvents = eventMap.get(target);
+        if (elementEvents) {
+          const handlers = elementEvents.get(eventType);
+          if (handlers) {
+            const handlerArray = Array.from(handlers);
+            for (const handler of handlerArray) {
+              if (typeof handler === "function") {
+                handler(e);
+                return;
+              }
+            }
+          }
+        }
+        target = target.parentElement;
+      }
+    });
+  });
+
+  container.setAttribute("data-event-listeners", "true");
+}
