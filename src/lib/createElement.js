c758659d2a230ca541c615ce750e5d04af443fc0
YeongseoYoon <l112xpla@gmail.com>
2025-07-12 23:52:27 +0900

    feat: updateAttributes 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -29,3 +29,24 @@ export function createElement(vNode) {
 
   return $el;
 }
+
+/**
+ * DOM 엘리먼트에 속성(props)을 적용합니다.
+ *
+ * @param {Element} $el - 속성을 적용할 DOM 엘리먼트
+ * @param {Object} props - 적용할 속성(props) 객체
+ */
+function updateAttributes($el, props) {
+  Object.entries(props).forEach(([attr, value]) => {
+    if (attr.startsWith("on") && typeof value === "function") {
+      const eventType = attr.toLowerCase().slice(2);
+      addEvent($el, eventType, value);
+    } else if (attr === "className") {
+      $el.setAttribute("class", value);
+    } else if (attr === "style" && typeof value === "object") {
+      Object.assign($el.style, value);
+    } else {
+      $el.setAttribute(attr, value);
+    }
+  });
+}
