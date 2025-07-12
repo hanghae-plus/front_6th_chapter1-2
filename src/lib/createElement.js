951e473f6d6e46c57ef39488deb3925994f07273
YeongseoYoon <l112xpla@gmail.com>
2025-07-12 23:51:54 +0900

    feat: createElement 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,31 @@
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+/**
+ * VNode(가상 노드)로부터 실제 DOM 엘리먼트를 생성합니다.
+ *
+ * @param {any} vNode - 가상 노드(VNode) 또는 텍스트/배열/null/boolean 등
+ * @returns {Node} 생성된 DOM 노드
+ */
+export function createElement(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
 
-function updateAttributes($el, props) {}
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
+
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    vNode.forEach((child) => fragment.appendChild(createElement(child)));
+    return fragment;
+  }
+
+  const $el = document.createElement(vNode.type);
+
+  updateAttributes($el, vNode.props ?? {});
+
+  $el.append(...vNode.children.map(createElement));
+
+  return $el;
+}
