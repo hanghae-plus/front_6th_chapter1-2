9f8ec389e0b678fa4e10f1692ab77ddfbd18f7a3
JiHoon-0330 <ljihoon0330@gmail.com>
2025-07-13 18:37:59 +0900

    feat: createVNode 구현하기
    Refs: #1

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,32 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props,
+    children: flattenRenderableChildren(children),
+  };
+}
+
+function isNotRenderable(child) {
+  return child == null || child === false;
+}
+
+function flattenRenderableChildren(children) {
+  const result = [];
+
+  const flatten = (value) => {
+    if (isNotRenderable(value)) {
+      return;
+    }
+
+    if (Array.isArray(value)) {
+      for (let i = 0; i < value.length; i++) {
+        flatten(value[i]);
+      }
+    } else {
+      result.push(value);
+    }
+  };
+
+  flatten(children);
+  return result;
 }
