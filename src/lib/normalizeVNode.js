62757f16009aed45bb21481ccae4cfbe3157b9b6
hmyo2853 <hmyo2853@gmail.com>
2025-07-14 01:40:42 +0900

    Feature : normalizeVNode 함수 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,32 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return "";
+  }
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return vNode.toString();
+  }
+  if (typeof vNode === "function") {
+    return vNode();
+  }
+
+  if (Array.isArray(vNode)) {
+    return vNode.map(normalizeVNode);
+  }
+  if (typeof vNode === "object") {
+    // 함수이면 계속 실행
+    if (typeof vNode.type === "function") {
+      const props = vNode.props;
+      const className = vNode.className;
+      const children = vNode.children;
+      return normalizeVNode(vNode.type({ ...props, ...className, children }));
+    }
+
+    // 함수가 아니라면 내부 children 을 재귀적으로 실행
+    return {
+      type: vNode.type,
+      props: vNode.props,
+      children: vNode.children.map((child) => normalizeVNode(child)).filter((child) => child !== ""),
+    };
+  }
+  return;
 }
