3233a2a4106a3f8c9f1f18c50723d626925198ea
Amelia-Shin <gmldnjs0402@naver.com>
2025-07-14 06:23:07 +0900

    normalizeVNode 작성 (정규화 표현식 미통과)

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,17 @@
 export function normalizeVNode(vNode) {
+  console.log("vNode", vNode);
+  
+  if (vNode == null || vNode == undefined || typeof(vNode) == "boolean") {
+    return "";
+  }
+
+  if (typeof(vNode) == "number") {
+    return String(vNode);
+  }
+
+  // TODO 정규화 표현 어떻게 구현해야하는가? 테스트 코드가 잘 이해되지 않음.
+  // if (typeof vNode.type === "function") {
+  // }
+  
   return vNode;
 }
