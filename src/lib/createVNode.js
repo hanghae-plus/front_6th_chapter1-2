f5c2c91bb56a830a3102af0fa5228fa7dbea8371
정석호 <creaticoding@gmail.com>
2025-07-14 03:46:37 +0900

    feat: createVNode 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,12 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props,
+    children: (children ?? [])
+      .flat(Infinity)
+      // normalizeVNode 으로 하면 "배열 렌더링"에서 실패함
+      // .map((x) => normalizeVNode(x))
+      .map((x) => (x == null || typeof x === "boolean" ? "" : x))
+      .filter((x) => x !== ""),
+  };
 }
