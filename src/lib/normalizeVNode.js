683d5e38d7d268039ee38a6164cd0c17ed0d8812
정석호 <creaticoding@gmail.com>
2025-07-14 03:46:20 +0900

    feat: normalizeVNode 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,43 @@
 export function normalizeVNode(vNode) {
+  if (vNode == null || typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  if (Array.isArray(vNode)) {
+    return vNode
+      .map(normalizeVNode)
+      .flat(Infinity)
+      .filter((x) => x !== "");
+  }
+
+  if (typeof vNode.type === "function") {
+    return normalizeVNode(
+      vNode.type({
+        children: (vNode.children ?? [])
+          .map(normalizeVNode)
+          .flat(Infinity)
+          .filter((x) => x !== ""),
+        ...(vNode.props ?? {}),
+      }),
+    );
+  }
+
+  if (typeof vNode === "object") {
+    const { type, props = {}, children } = vNode;
+
+    return {
+      type,
+      props,
+      children: (children ?? [])
+        .map(normalizeVNode)
+        .flat(Infinity)
+        .filter((x) => x !== ""),
+    };
+  }
+
   return vNode;
 }
