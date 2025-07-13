cea2f913429a35abdf254c5325a5fdec2e2aa227
areumH <dkfmal0109@gmail.com>
2025-07-13 19:12:27 +0900

    feat: createElement 함수 작성

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,34 @@
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+// VirtualDOM을 RealDOM으로 변환
+// vNode = { type, props, children: children.flat() }
+export function createElement(vNode) {
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
 
-function updateAttributes($el, props) {}
+  const $el = document.createElement(vNode.type);
+  updateAttributes($el, vNode.props ?? {});
+
+  const children = vNode.children.map(createElement);
+  children.map((child) => $el.appendChild(child));
+
+  return $el;
+}
+
+// 새 DOM 노드에 속성 세팅
+// prpos - type, className, boolean, handler
+function updateAttributes($el, props) {
+  Object.entries(props).forEach(([attr, value]) => {
+    if (attr === "className") {
+      $el.setAttribute("class", value);
+    } else if (typeof attr === "boolean") {
+      attr && $el.setAttribute(attr, "");
+    } else if (attr.startsWith("on")) {
+      const eventType = attr.slice(2).toLowerCase();
+      addEvent($el, eventType, value);
+    } else {
+      $el.setAttribute(attr, value);
+    }
+  });
+}
