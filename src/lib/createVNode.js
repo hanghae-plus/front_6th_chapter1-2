0a3a387bd5f0f5451bb8f4c515e009257aa872d5
devchangjun <ckdwns9121@naver.com>
2025-07-13 17:21:24 +0900

    feat: children 평탄화 작업

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,7 +1,9 @@
 export function createVNode(type, props, ...children) {
+  console.log("여기");
+  console.log(children);
   return {
     type,
     props,
-    children,
+    children: children.flat(Infinity),
   };
 }
