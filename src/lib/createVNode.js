7c0f5b4ce2a09ec0a5b034b82407d73ca9f13bf6
Angie Lee <angielxx94@gmail.com>
2025-07-13 19:50:51 +0900

    feat: createVNode 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,22 @@
+/**
+ * 가상 돔 노드 생성
+ * @param {*} type
+ * @param {*} props
+ * @param  {...any} children
+ * @returns {Object} {type, props, children}
+ */
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props,
+    children: children
+      .flat(Infinity)
+      .filter(
+        (child) =>
+          child !== null &&
+          child !== undefined &&
+          child !== false &&
+          child !== true,
+      ),
+  };
 }
