44ac4195305c6b6bb40471772e6bd6f62fadb1ff
tksemf7410 <tksemf7410@gmail.com>
2025-07-15 03:06:08 +0900

    feat: normalizeVNode 함수 완성

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,22 @@
 export function normalizeVNode(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return vNode.toString();
+  }
+
+  if (typeof vNode.type === "function") {
+    return normalizeVNode(
+      vNode.type({
+        ...vNode.props,
+        children: vNode.children.flat(Infinity).map(normalizeVNode),
+      }),
+    );
+  } else {
+    vNode.children = vNode.children.filter((x) => x !== null && x !== undefined && typeof x !== "boolean");
+  }
+
   return vNode;
 }
