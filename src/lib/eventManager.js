e58fb1869c23d582516c4c960c5b40eab182c65f
Amelia-Shin <gmldnjs0402@naver.com>
2025-07-15 05:18:03 +0900

    addEvent 추가

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,20 @@
-export function setupEventListeners(root) {}
+// element -> eventType -> Set(handlers)
+// WeakMap 은 메모리 누수 방지 & 은밀하게 데이터를 저장하고 싶을 떄
+const eventMap = new WeakMap();
 
-export function addEvent(element, eventType, handler) {}
+// export function setupEventListeners(root) {}
 
-export function removeEvent(element, eventType, handler) {}
+export function addEvent(element, eventType, handler) {
+    if (!eventMap.has(element)) {
+        eventMap.set(element, new Map());
+    }
+
+    const elementEvents = eventMap.get(element);
+    if (!elementEvents.has(eventType)) {
+        elementEvents.set(eventType, new Set());
+    }
+
+    elementEvents.get(eventType).add(handler);    
+}
+
+// export function removeEvent(element, eventType, handler) {}
