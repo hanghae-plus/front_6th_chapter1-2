c33160c2c360d0311ea406b4f238ea7801d6e722
JHeeJinDev <zqewe12@gmail.com>
2025-07-14 14:08:01 +0900

    feat: createVNode 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,7 +1,13 @@
 export function createVNode(type, props, ...children) {
+  // props가 null 또는 undefined면 null로 통일
+  props = props == null ? null : props;
+
+  // children 평탄화 및 Falsy 값 제거
+  const flatChildren = children.flat(Infinity).filter((c) => c !== null && c !== undefined && c !== false);
+
   return {
     type,
     props,
-    children: Array.isArray(children) ? children : [children],
+    children: flatChildren,
   };
 }
