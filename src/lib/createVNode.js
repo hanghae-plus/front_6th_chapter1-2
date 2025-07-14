02cb79eebf093fefa05f0e81cd56604d7b2337f9
BangDori <qudwns8865@naver.com>
2025-07-14 15:15:21 +0900

    createVNode 구현
    - 가독성을 위해 조건문을 분리
    - nullish 연산자를 활용해 nullish props 값 null 처리
    - JSX children 중첩 단계를 고려하여 Infinity로 평탄화

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,15 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  const flatChildren = children.flat(Infinity).filter((child) => {
+    if (child === null) return false;
+    if (child === undefined) return false;
+    if (child === false) return false;
+
+    return true;
+  });
+
+  return {
+    type,
+    props: props ?? null, // nullish한 값이 들어오는 경우 null 처리
+    children: flatChildren,
+  };
 }
