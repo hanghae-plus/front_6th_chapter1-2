74b87aae5ce1c984c4a8e36e48fe4b4dcb7f91b4
JangRuBin2 <wkdfnqls2465@gmail.com>
2025-07-14 23:28:58 +0900

    feat: 정규화 통과... vNode object, type이 컴포넌트의 경우 정규화 재진행. 그 외에는 children 평탄화

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,4 +1,28 @@
 export function normalizeVNode(vNode) {
-  vNode = !vNode ? "" : vNode;
+  if (typeof vNode === "boolean") return "";
+  if (typeof vNode === "number" || typeof vNode === "string") return String(vNode);
+  if (!vNode) return "";
+
+  if (typeof vNode === "object") {
+    // 컴포넌트일 때 다시 정규화 진행
+    if (typeof vNode.type === "function") {
+      const renderedVNode = vNode.type(vNode.props || {});
+      return normalizeVNode(renderedVNode);
+    }
+
+    // 일반 VNode면 children 정규화
+    return {
+      ...vNode,
+      children: normalizeChildren(vNode.children),
+    };
+  }
+
   return vNode;
+}
+
+export function normalizeChildren(children) {
+  return (children || [])
+    .flat(Infinity)
+    .map(normalizeVNode)
+    .filter((child) => child !== "");
 }
