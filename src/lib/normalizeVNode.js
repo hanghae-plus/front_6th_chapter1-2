ec5cf8456ffbf0bb88a08c7bd065fe8041bdb5f8
nimusmix <nimusmix@gmail.com>
2025-07-15 01:43:00 +0900

    feat: normalizeVNode 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,33 @@
+import { isArray, isBoolean, isEmptyString, isFunction, isNil, isNumber, isString } from "../utils";
+
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (isNil(vNode) || isBoolean(vNode)) {
+    return "";
+  }
+
+  if (isString(vNode)) {
+    return vNode;
+  }
+
+  if (isNumber(vNode)) {
+    return vNode.toString();
+  }
+
+  if (isArray(vNode)) {
+    return vNode.map(normalizeVNode).filter((v) => !isEmptyString(v));
+  }
+
+  if (isFunction(vNode.type)) {
+    const props = { ...(vNode.props ?? {}), children: vNode.children };
+    return normalizeVNode(vNode.type(props));
+  }
+
+  return {
+    ...vNode,
+    children: normalizeChildren(vNode.children),
+  };
 }
+
+const normalizeChildren = (children) => {
+  return isArray(children) ? children.map(normalizeVNode).filter((c) => !isEmptyString(c)) : normalizeVNode(children);
+};
