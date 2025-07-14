3755daa68180a3661b0eeabc1a80482d85ac17aa
Amelia-Shin <gmldnjs0402@naver.com>
2025-07-15 01:59:14 +0900

    normalizeVNode 구현(컴포넌트 정규화)

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,17 +1,55 @@
-export function normalizeVNode(vNode) {
-  console.log("vNode", vNode);
-  
+export function normalizeVNode(vNode) {  
   if (vNode == null || vNode == undefined || typeof(vNode) == "boolean") {
     return "";
   }
-
-  if (typeof(vNode) == "number") {
+  
+  if (typeof(vNode) == "number" || typeof(vNode) == "string") {
     return String(vNode);
   }
-
-  // TODO 정규화 표현 어떻게 구현해야하는가? 테스트 코드가 잘 이해되지 않음.
-  // if (typeof vNode.type === "function") {
-  // }
   
+  if (typeof vNode.type === "function") {
+    // Component 는 인자 값을 props 로 받음.
+    // props 에 children 정보를 보내서 함수를 실행 시킴 
+    // type이 컴포넌트 -> (함수의 역할) , 컴포넌트를 실행하기 위해선 <Component() /> 처럼 실행
+    // 이런 형태로 children 이라는 인자를 넘겨주기 UnorderedList({children : vNode.children})
+    // {
+    //   type: [Function: UnorderedList],
+    //   props: null,
+    //   children: [
+    //     { type: [Function: ListItem], props: [Object], children: [Array] },
+    //     { type: [Function: ListItem], props: [Object], children: [Array] },
+    //     { type: [Function: ListItem], props: [Object], children: [Array] }
+    //   ]
+    // }
+    // 다시 정규화를 진행하면 vNode는 아래와 같은 형태가 된다.
+    // <ul {...{}}>
+    //   { type: [Function: ListItem], props: [Object], children: [Array] },
+    //   { type: [Function: ListItem], props: [Object], children: [Array] },
+    //   { type: [Function: ListItem], props: [Object], children: [Array] }
+    // </ul>,
+    return normalizeVNode(vNode.type({ ...vNode.props, children : vNode.children }));
+  }
+  
+  /*
+  {
+    type: 'ul',
+    props: {},
+    children: [
+      { type: [Function: ListItem], props: [Object], children: [Array] },
+      { type: [Function: ListItem], props: [Object], children: [Array] },
+      { type: [Function: ListItem], props: [Object], children: [Array] }
+    ]
+  }
+  */
+  if (typeof vNode.type === "string") {
+    let normalizedChildren = [];
+    normalizedChildren = vNode.children.map(normalizeVNode);
+
+    return {
+      type: vNode.type,
+      props: vNode.props || null,
+      children: normalizedChildren,
+    };
+  }
   return vNode;
 }
