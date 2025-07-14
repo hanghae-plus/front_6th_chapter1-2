edb77f13993ce205cf67adf7513cc06772fef1e8
JiHoon-0330 <ljihoon0330@gmail.com>
2025-07-14 22:51:41 +0900

    feat: eventManager
    Refs: #4

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,26 @@
-export function setupEventListeners(root) {}
+const events = {};
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  for (const eventType in events) {
+    root.addEventListener(eventType, (e) => {
+      events[eventType].forEach(({ element, handler }) => {
+        if (element.contains(e.target)) {
+          handler(e);
+        }
+      });
+    });
+  }
+}
 
-export function removeEvent(element, eventType, handler) {}
+export function addEvent(element, eventType, handler) {
+  events[eventType] ??= new Set();
+  events[eventType].add({ element, handler });
+}
+
+export function removeEvent(element, eventType, handler) {
+  events[eventType]?.forEach((item) => {
+    if (item.element === element && item.handler === handler) {
+      events[eventType].delete(item);
+    }
+  });
+}
