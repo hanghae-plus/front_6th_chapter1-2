3ef7f9ee742bb269e12e9cb290064657e911b441
devchangjun <ckdwns9121@naver.com>
2025-07-15 00:45:36 +0900

    fix: 이전 virtureDom을 저장해서 최신꺼랑 비교

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -3,19 +3,32 @@ import { createElement } from "./createElement";
 import { updateElement } from "./updateElement";
 import { normalizeVNode } from "./normalizeVNode";
 
+// 컨테이너별 이전 Virtual DOM을 저장하는 WeakMap
+const previousVNodes = new WeakMap();
+
 export function renderElement(vNode, container) {
   // 함수형 컴포넌트 정규화
   const normalizedVNode = normalizeVNode(vNode);
 
+  // 이전 Virtual DOM 가져오기
+  const previousVNode = previousVNodes.get(container);
+
   // 이미 컨테이너 안에 DOM이 있다면 업데이트
-  if (container.firstChild) {
-    updateElement(container, normalizedVNode, container.firstChild);
+  if (container.firstChild && previousVNode) {
+    updateElement(container, normalizedVNode, previousVNode, 0);
   }
   // 없으면 DOM 생성하고 container에 붙임
   else {
+    // 기존 자식들 제거
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
     const $el = createElement(normalizedVNode);
     container.appendChild($el);
   }
+
+  // 현재 Virtual DOM 저장
+  previousVNodes.set(container, normalizedVNode);
 
   // container에 이벤트 리스너 등록
   setupEventListeners(container);
