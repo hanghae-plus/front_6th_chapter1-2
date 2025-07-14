39133f04817bcf122ed338021f4dbd3c1fbdb96d
devchangjun <ckdwns9121@naver.com>
2025-07-14 22:52:14 +0900

    fix: 함수형 컴포넌트 정규화 로직 추가

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,15 +1,19 @@
 import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { updateElement } from "./updateElement";
+import { normalizeVNode } from "./normalizeVNode";
 
 export function renderElement(vNode, container) {
+  // 함수형 컴포넌트 정규화
+  const normalizedVNode = normalizeVNode(vNode);
+
   // 이미 컨테이너 안에 DOM이 있다면 업데이트
   if (container.firstChild) {
-    updateElement(vNode, container.firstChild);
+    updateElement(container, normalizedVNode, container.firstChild);
   }
   // 없으면 DOM 생성하고 container에 붙임
   else {
-    const $el = createElement(vNode);
+    const $el = createElement(normalizedVNode);
     container.appendChild($el);
   }
 
