30c1943e69c00193e68c0f6e5aa211ae571dc9f0
devchangjun <ckdwns9121@naver.com>
2025-07-14 17:21:03 +0900

    feat: 이벤트 리스너 등록

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,23 @@
-export function setupEventListeners(root) {}
+// eslint-disable-next-line no-unused-vars
+export function setupEventListeners(_root) {
+  // TODO: Implement event listener setup for root element
+}
 
-export function addEvent(element, eventType, handler) {}
+/**
+ *
+ * {
+ * type: "div",
+ * props: {
+ *   onClick: () => alert("클릭됨!"),
+ *   children: "버튼"
+ * }
+ * }
+ * 이런 형태로 들어올껀데 여기서 이벤트 리스너를 등록해야 함.
+ */
+export function addEvent(element, eventType, handler) {
+  element.addEventListener(eventType, handler);
+}
 
-export function removeEvent(element, eventType, handler) {}
+export function removeEvent(element, eventType, handler) {
+  element.removeEventListener(eventType, handler);
+}
