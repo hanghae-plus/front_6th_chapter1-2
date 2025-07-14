45d3990052208ede3162396bec18399580b83ce4
hmyo2853 <hmyo2853@gmail.com>
2025-07-15 00:32:58 +0900

    Refactor : 코드 리팩토링

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -12,21 +12,17 @@ export function normalizeVNode(vNode) {
   if (Array.isArray(vNode)) {
     return vNode.map(normalizeVNode);
   }
-  if (typeof vNode === "object") {
-    // 함수이면 계속 실행
-    if (typeof vNode.type === "function") {
-      const props = vNode.props;
-      const className = vNode.className;
-      const children = vNode.children;
-      return normalizeVNode(vNode.type({ ...props, ...className, children }));
-    }
-
-    // 함수가 아니라면 내부 children 을 재귀적으로 실행
-    return {
-      type: vNode.type,
-      props: vNode.props,
-      children: vNode.children.map((child) => normalizeVNode(child)).filter((child) => child !== ""),
-    };
+  // 함수이면 계속 실행
+  if (typeof vNode.type === "function") {
+    const props = vNode.props;
+    const children = vNode.children;
+    return normalizeVNode(vNode.type({ ...props, children }));
   }
-  return;
+
+  // 함수가 아니라면 내부 children 을 재귀적으로 실행
+  return {
+    type: vNode.type,
+    props: vNode.props,
+    children: vNode.children.map((child) => normalizeVNode(child)).filter((child) => child !== ""),
+  };
 }
