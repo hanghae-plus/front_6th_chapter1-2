39e56adfdc20b4aae6085d7e4ca2052b29356f79
JangRuBin2 <wkdfnqls2465@gmail.com>
2025-07-15 00:35:59 +0900

    refactor: 정규화 함수 리팩토링

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,28 +1,17 @@
 export function normalizeVNode(vNode) {
-  if (typeof vNode === "boolean") return "";
-  if (typeof vNode === "number" || typeof vNode === "string") return String(vNode);
-  if (!vNode) return "";
-
-  if (typeof vNode === "object") {
-    // 컴포넌트일 때 다시 정규화 진행
-    if (typeof vNode.type === "function") {
-      const renderedVNode = vNode.type(vNode.props || {});
-      return normalizeVNode(renderedVNode);
-    }
-
-    // 일반 VNode면 children 평탄화
-    return {
-      ...vNode,
-      children: normalizeChildren(vNode.children),
+  if (vNode == null || typeof vNode === "boolean") return "";
+  if (typeof vNode === "string" || typeof vNode === "number") return String(vNode);
+  if (typeof vNode.type === "function") {
+    const props = {
+      ...vNode.props,
+      children: vNode.children,
     };
+    const renderedVNode = vNode.type(props);
+    return normalizeVNode(renderedVNode);
   }
-
-  return vNode;
-}
-
-export function normalizeChildren(children) {
-  return (children || [])
-    .flat(Infinity)
-    .map(normalizeVNode)
-    .filter((child) => child !== "");
+  return {
+    type: vNode.type,
+    props: vNode.props || null,
+    children: vNode.children.map(normalizeVNode).filter(Boolean),
+  };
 }
