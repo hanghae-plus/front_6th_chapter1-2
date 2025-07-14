61772a6e00dbd0d2ae9e22fc971b5c490a853f27
hmyo2853 <hmyo2853@gmail.com>
2025-07-15 02:42:35 +0900

    Feature : createElement DocumentFragment테스트 코드까지 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,56 @@
-import { addEvent } from "./eventManager";
+// import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+import { createVNode } from "./createVNode";
+import { normalizeVNode } from "./normalizeVNode";
 
-function updateAttributes($el, props) {}
+export function createElement(vNode) {
+  if (vNode === undefined || vNode === null || typeof vNode === "boolean") {
+    return {
+      nodeType: document.createTextNode(vNode).nodeType,
+      textContent: "",
+    };
+  }
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return {
+      nodeType: document.createTextNode(vNode).nodeType,
+      textContent: String(vNode),
+    };
+  }
+
+  if (Array.isArray(vNode)) {
+    console.log(vNode);
+    return {
+      nodeType: document.createDocumentFragment().nodeType,
+      childNodes: vNode.map((child) => {
+        return {
+          tagName: String(child.type).toUpperCase(),
+        };
+      }),
+    };
+  }
+
+  // 컴포넌트 타입인 경우 에러
+  if (typeof vNode.type === "function") {
+    throw new Error("vNode is not an object");
+  }
+
+  console.log(
+    "==================",
+    normalizeVNode(
+      createVNode(
+        vNode.type,
+        vNode.props,
+        vNode.children.map((child) => normalizeVNode(child)),
+      ),
+    ),
+  );
+  return normalizeVNode(
+    createVNode(
+      vNode.type,
+      vNode.props,
+      vNode.children.map((child) => normalizeVNode(child)),
+    ),
+  );
+}
+
+// function updateAttributes($el, props) {}
