b07dad2e7f4f83e412a6e01742e7873b6aa523fe
Angie Lee <angielxx94@gmail.com>
2025-07-14 21:07:26 +0900

    feat: createElement 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,4 +1,4 @@
-// import { addEvent } from "./eventManager";
+import { addEvent } from "./eventManager";
 
 /**
  * @param {object} vNode { type, props, children }
@@ -13,13 +13,114 @@ export function createElement(vNode) {
     return document.createTextNode(String(vNode));
   }
 
-  // "배열 입력에 대해 DocumentFragment를 생성해야 한다"
   if (Array.isArray(vNode)) {
-    const fragment = document.createDocumentFragment();
-    vNode.forEach((child) => {
-      fragment.appendChild(createElement(child));
+    const fragment = new DocumentFragment();
+
+    vNode.forEach((node) => {
+      const element = document.createElement(node.type);
+      element.textContent = node.children;
+
+      fragment.appendChild(element);
     });
+
+    return fragment;
   }
+
+  if (typeof vNode === "object" && typeof vNode.type === "function") {
+    throw new Error(
+      "컴포넌트는 createElement로 처리할 수 없습니다. 정규화 후 createElement를 사용해주세요.",
+    );
+  }
+
+  const element = document.createElement(vNode.type);
+  const { props, children } = vNode;
+
+  updateAttributes(element, props);
+  children.forEach((child) => element.appendChild(createElement(child)));
+
+  return element;
 }
 
-// function updateAttributes($el, props) {}
+/**
+ * 속성 업데이트
+ * @param {*} $el { HTMLElement }
+ * @param {*} props { ...attributes }
+ */
+function updateAttributes($el, props) {
+  if (!props) {
+    return;
+  }
+
+  Object.entries(props).forEach(([key, value]) => {
+    // 이벤트 핸들러인 경우 (ex. onClick, onMouseOver, etc.)
+    if (key.startsWith("on") && typeof value === "function") {
+      const eventType = key.slice(2).toLowerCase();
+      addEvent($el, eventType, value);
+
+      return;
+    }
+
+    // className인 경우
+    if (key === "className") {
+      if (value) {
+        $el.setAttribute("class", value);
+      } else {
+        $el.removeAttribute("class");
+      }
+      $el.removeAttribute("classname");
+
+      return;
+    }
+
+    // style인 경우
+    if (key === "style") {
+      // 기존 스타일은 유지하면서 새로운 스타일만 추가/덮어쓰기
+      Object.assign($el.style, value);
+
+      return;
+    }
+
+    // checked, disabled, selected, readOnly인 경우
+    if (["checked", "disabled", "selected", "readOnly"].includes(key)) {
+      $el.setAttribute(key, value);
+
+      if (!value) {
+        $el.removeAttribute(key);
+      }
+
+      return;
+    }
+
+    $el.setAttribute(key, value);
+  });
+}
+
+/**
+Expected
+
+<ul>
+  <li id="item-1" class="list-item ">
+    - Item 1
+  </li>
+  <li id="item-2" class="list-item ">
+    - Item 2
+  </li>
+  <li id="item-3" class="list-item last-item">
+    - Item 3
+  </li>
+</ul>;
+
+Result
+
+<ul>
+  <li id="item-1" class="list-item " classname="list-item ">
+    - Item 1
+  </li>
+  <li id="item-2" class="list-item " classname="list-item ">
+    - Item 2
+  </li>
+  <li id="item-3" class="list-item last-item" classname="list-item last-item">
+    - Item 3
+  </li>
+</ul>;
+*/
