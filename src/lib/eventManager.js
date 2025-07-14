0d1df1f1710a2d8dd60720922c88cd75b74f43e3
areumH <dkfmal0109@gmail.com>
2025-07-15 03:31:15 +0900

    fix: 이벤트 위임 방식으로 eventManager 함수 수정

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,26 +1,38 @@
-// root 아래의 모든 DOM에 이벤트 연결
+// 전역 이벤트 저장소
+const eventStore = new Map();
+
+// 렌더링 후 전역 이벤트 등록
 export function setupEventListeners(root) {
-  const elements = root.querySelectorAll("*");
+  function handleEvent(event) {
+    // WeakMap 객체
+    const handlerMap = eventStore[event.type];
+    // 이벤트 발생 요소
+    const target = event.target;
 
-  elements.forEach((el) => {
-    const props = el.vDOMProps;
+    if (!handlerMap || !handlerMap.has(target)) return;
 
-    if (!props) return;
-    Object.entries(props).forEach(([attr, handler]) => {
-      if (attr.startsWith("on")) {
-        const eventType = attr.slice(2).toLowerCase();
-        addEvent(el, eventType, handler);
-      }
-    });
-  });
+    const handler = handlerMap.get(target);
+    handler(event);
+  }
+
+  // 전역 이벤트 저장소의 이벤트 등록
+  for (const eventType in eventStore) {
+    root.removeEventListener(eventType, handleEvent);
+    root.addEventListener(eventType, handleEvent);
+  }
 }
 
-// 특정 요소에 이벤트 등록
+// 이벤트 위임 - 이벤트 저장 및 등록
 export function addEvent(element, eventType, handler) {
-  element.addEventListener(eventType, handler);
+  if (!element || typeof handler != "function") return;
+
+  eventStore[eventType] = eventStore[eventType] || new WeakMap();
+  eventStore[eventType].set(element, handler);
 }
 
-// 특정 요소에 이벤트 삭제
+// 전역 상태의 이벤트 삭제
 export function removeEvent(element, eventType, handler) {
-  element.removeEventListener(eventType, handler);
+  if (eventStore[eventType].get(element) === handler) {
+    eventStore[eventType].delete(element);
+  }
 }
