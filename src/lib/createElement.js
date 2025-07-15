b8f14e80b7e8f777e4ea8c45ce20a3cba6b56f44
JangRuBin2 <wkdfnqls2465@gmail.com>
2025-07-15 23:41:33 +0900

    feat: createElement구현, 컴포넌트 예외처리는 테스트에 안뜸

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,36 @@
-import { addEvent } from "./eventManager";
+export function createElement(vNode) {
+  if (vNode == null || typeof vNode === "boolean") return document.createTextNode("");
 
-export function createElement(vNode) {}
+  if (typeof vNode === "string" || typeof vNode === "number") return document.createTextNode(String(vNode));
 
-function updateAttributes($el, props) {}
+  if (typeof vNode === "function") throw new Error("컴포넌트는 안돼용");
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    vNode.forEach((child) => {
+      fragment.appendChild(createElement(child));
+    });
+    return fragment;
+  }
+  const $el = document.createElement(vNode.type);
+  updateAttributes($el, vNode.props);
+  (vNode.children || []).forEach((child) => {
+    $el.appendChild(createElement(child));
+  });
+  return $el;
+}
+
+function updateAttributes($el, props) {
+  if (!props) return;
+
+  Object.entries(props).forEach(([key, value]) => {
+    if (key === "className") {
+      $el.setAttribute("class", value);
+    } else if (key.startsWith("data-")) {
+      $el.setAttribute(key, value);
+    } else if (key in $el) {
+      $el[key] = value;
+    } else {
+      $el.setAttribute(key, value);
+    }
+  });
+}
