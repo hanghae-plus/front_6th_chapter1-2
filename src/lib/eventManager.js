1a4a1202452c4d8ec8693e24ef01edbfac628997
JangRuBin2 <wkdfnqls2465@gmail.com>
2025-07-16 02:34:07 +0900

    refactor: 이벤트 매니저 리팩토링 > 가은님 감사합니다!

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,37 +1,30 @@
 //가비지 컬렉션 되면 자동제거
-const eventStore = new WeakMap();
+const eventStore = new Map();
 
 export function setupEventListeners(root) {
-  root.addEventListener("click", (e) => {
-    let target = e.target;
-    // console.log("target:", target);
-    if (target) {
-      const handlers = eventStore.get(target);
-      // console.log(handlers);
-      if (handlers && handlers["click"]) {
-        handlers["click"].forEach((fn) => fn(e));
-      }
-      // console.log(e.cancelBubble);
-      if (e.cancelBubble) return; // stopPropagation
-      target = target.parentNode;
-    }
-  });
+  console.log(eventStore);
+
+  for (const event in eventStore) {
+    root.addEventListener(event, (e) => {
+      // console.log(event);
+      eventStore[event].forEach(({ element, handler }) => {
+        if (element.contains(e.target)) handler(e);
+      });
+    });
+  }
 }
 
 export function addEvent(element, eventType, handler) {
-  if (!eventStore.has(element)) {
-    eventStore.set(element, {});
-  }
-  const handlers = eventStore.get(element);
-
-  if (!handlers[eventType]) {
-    handlers[eventType] = new Set();
-  }
-
-  handlers[eventType].add(handler);
-  console.log(handlers);
+  if (!eventStore[eventType]) eventStore[eventType] = new Set();
+  eventStore[eventType].add({ element, handler });
 }
 
 export function removeEvent(element, eventType, handler) {
-  console.log(element, handler, eventType);
+  if (eventStore[eventType]) {
+    eventStore[eventType].forEach((item) => {
+      if (item.element === element && item.handler === handler) {
+        eventStore[eventType].delete(item);
+      }
+    });
+  }
 }
