d1ab05358964293869e57e2a38f6b6dbaa94425d
tksemf7410 <tksemf7410@gmail.com>
2025-07-16 01:16:40 +0900

    feat: createElement 함수 완성

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,39 @@
-import { addEvent } from "./eventManager";
+export function createElement(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
 
-export function createElement(vNode) {}
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode.toString());
+  }
 
-function updateAttributes($el, props) {}
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    vNode.forEach((child) => {
+      fragment.appendChild(createElement(child));
+    });
+    return fragment;
+  }
+
+  const $el = document.createElement(vNode.type);
+  updateAttributes($el, vNode.props ?? {});
+
+  vNode.children.forEach((child) => {
+    $el.appendChild(createElement(child));
+  });
+
+  return $el;
+}
+
+function updateAttributes($el, props) {
+  Object.entries(props).forEach(([key, value]) => {
+    if (key === "className") {
+      $el.setAttribute("class", value);
+    } else if (key.startsWith("on") && typeof value === "function") {
+      const event = key.slice(2).toLowerCase(); // onClick -> click
+      $el.addEventListener(event, value);
+    } else {
+      $el.setAttribute(key, value);
+    }
+  });
+}
