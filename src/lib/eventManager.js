6508ec4702c9bc5229ea62b83cfa7d49a6051702
JHeeJinDev <zqewe12@gmail.com>
2025-07-15 18:38:38 +0900

    feat: eventManager 구현

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,47 @@
-export function setupEventListeners(root) {}
+// 이벤트 관리 저장소
+const eventStore = new WeakMap(); // 엘리먼트 기준으로 이벤트 핸들러 저장
+const allEvents = new Set(); // 등록된 이벤트 타입 추적 (중복 방지)
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  // 모든 이벤트 타입을 순회하며 이벤트 리스너를 설정
+  allEvents.forEach((eventType) => {
+    root.addEventListener(eventType, (event) => {
+      const target = event.target;
+      const elementEvents = eventStore.get(target);
 
-export function removeEvent(element, eventType, handler) {}
+      if (!elementEvents || !elementEvents[eventType]) return;
+
+      elementEvents[eventType].forEach((handler) => {
+        handler(event);
+      });
+    });
+  });
+}
+
+export function addEvent(element, eventType, handler) {
+  console.log("addEvent", element, eventType, handler);
+  if (!eventStore.has(element)) {
+    eventStore.set(element, {});
+  }
+
+  const elementEvents = eventStore.get(element);
+
+  if (!elementEvents[eventType]) {
+    elementEvents[eventType] = new Set();
+  }
+
+  elementEvents[eventType].add(handler);
+
+  allEvents.add(eventType);
+}
+
+export function removeEvent(element, eventType, handler) {
+  const elementEvents = eventStore.get(element);
+
+  if (!elementEvents) return;
+
+  const handlers = elementEvents[eventType];
+  if (!handlers) return;
+
+  handlers.delete(handler);
+}
