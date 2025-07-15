19574925aa1804e9e9b3fcee3610700a360470ac
hmyo2853 <hmyo2853@gmail.com>
2025-07-16 01:49:25 +0900

    Feature : setupEventListeners, addEvent, removeEvent 개선

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,32 +1,36 @@
-// 객체별 이벤트 저장소
+// 이벤트 저장소
+// 이벤트 자체를 객체로 사용할 수 있으므로 Map으로 관리
 const eventRegistry = new Map();
 
 export function setupEventListeners(root) {
-  // 클릭 이벤트 위임
-  root.addEventListener("click", (e) => {
-    eventRegistry.forEach((eventMap, element) => {
-      if (element === e.target) {
-        eventMap.forEach((handler) => {
-          // 이벤트 실행시 event를 전달
-          // e.preventDefault나 e.stopPropagation을 통해 이벤트 전파를 막는 용도
+  // root인 상위 엘리먼트에 이벤트리스너 등록
+  for (const event in eventRegistry) {
+    root.addEventListener(event, (e) => {
+      eventRegistry[event].forEach(({ element, handler }) => {
+        if (element.contains(e.target)) {
           handler(e);
-        });
-      }
+        }
+      });
     });
-  });
+  }
 }
 
 export function addEvent(element, eventType, handler) {
   // 모든 이벤트는 이벤트 위임 방식으로 setupEventListeners에서 처리해야함
   // 해당 이벤트가 없다면 생성
-  if (!eventRegistry.has(element)) {
-    eventRegistry.set(element, new Map());
+  if (!eventRegistry[eventType]) {
+    eventRegistry[eventType] = new Set();
   }
-
-  const eventMap = eventRegistry.get(element);
-  eventMap.set(eventType, handler);
-
-  return element;
+  eventRegistry[eventType].add({ element, handler });
 }
 
-// export function removeEvent(element, eventType, handler) {}
+export function removeEvent(element, eventType, handler) {
+  // 내부 이벤트를 순회하면서 등록되어 있는 이벤트가 있으면 해당 이벤트 삭제
+  if (eventRegistry[eventType]) {
+    eventRegistry[eventType].forEach((item) => {
+      if (item.element === element && item.handler === handler) {
+        eventRegistry[eventType].delete(item);
+      }
+    });
+  }
+}
