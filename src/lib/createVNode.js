7269e06bd4505e37a251679ae02eba29393c3a5d
BangDori <qudwns8865@naver.com>
2025-07-16 01:35:25 +0900

    createVNode 함수 리팩토링

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,15 +1,21 @@
+/**
+ * 가상 노드(vNode)를 생성하는 함수
+ * - children 배열 하위 요소의 타입이 null, undefined, boolean인 경우에는 제거
+ *
+ * @param {string} type
+ * @param {object | null} props
+ * @param  {...any} children
+ * @returns {any} 가상 노드
+ */
 export function createVNode(type, props, ...children) {
-  const flatChildren = children.flat(Infinity).filter((child) => {
-    if (child === null) return false;
-    if (child === undefined) return false;
-    if (child === false) return false;
-
-    return true;
-  });
-
   return {
     type,
-    props: props ?? null, // nullish한 값이 들어오는 경우 null 처리
-    children: flatChildren,
+    props,
+    children: children.flat(Infinity).filter((child) => {
+      if (child === null || child === undefined) return false;
+      if (typeof child === "boolean") return false;
+
+      return true;
+    }),
   };
 }
