ebde0ad367325f17381129182eecc15ce99e1130
Amelia-Shin <gmldnjs0402@naver.com>
2025-07-16 01:51:49 +0900

    setupEventListeners 추가

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,8 +1,33 @@
 // element -> eventType -> Set(handlers)
-// WeakMap 은 메모리 누수 방지 & 은밀하게 데이터를 저장하고 싶을 떄
+// WeakMap 은 메모리 누수 방지용
 const eventMap = new WeakMap();
+const delegatedEvents = new Set();
+let rootElement = null;
 
-// export function setupEventListeners(root) {}
+export function setupEventListeners(container) {
+    rootElement = container;
+    delegatedEvents.forEach((eventType) => {
+        // handleEvent 를 풀어 쓰자면... (event) => handleEvent(event)
+        container.removeEventListener(eventType, handleEvent);
+        container.addEventListener(eventType, handleEvent);
+    });
+}
+
+function handleEvent(event) {
+    let target = event.target;
+
+    while (target && target !== rootElement) {
+        const elementEvents = eventMap.get(target);
+        if (elementEvents) {
+            const handlers = elementEvents.get(event.type);
+            if (handlers) {
+                handlers.forEach((handler) => handler(event));
+                return;
+            }
+        }
+        target = target.parentElement;
+    }
+}
 
 export function addEvent(element, eventType, handler) {
   if (!eventMap.has(element)) {
@@ -15,14 +40,32 @@ export function addEvent(element, eventType, handler) {
   }
 
   elementEvents.get(eventType).add(handler);
+  /**
+   * elementEvents 형태
+   * Map(1) {
+   *    'click' => Set(1) {
+   *        [Function: spy] {
+   *            getMockName: [Function (anonymous)],
+   *            mockName: [Function (anonymous)],
+   *            ... 
+  */
+
+  if (!delegatedEvents.has(eventType)) {
+    delegatedEvents.add(eventType);
+
+    if (rootElement) {
+        rootElement.removeEventListener(eventType, handleEvent);
+        rootElement.addEventListener(eventType, handleEvent);
+    }
+  }
 }
 
 export function removeEvent(element, eventType, handler) {
-    const elementEvents = eventMap.get(element);
-    if (!elementEvents) return;
+  const elementEvents = eventMap.get(element);
+  if (!elementEvents) return;
 
-    const handlers = elementEvents.get(eventType);
-    if (!handlers) return;
+  const handlers = elementEvents.get(eventType);
+  if (!handlers) return;
 
-    handlers.delete(handler);
+  handlers.delete(handler);
 }
