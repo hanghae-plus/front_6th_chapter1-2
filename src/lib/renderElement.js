4a5555e45281b3e4d4609ddb523b628699ca5044
BangDori <qudwns8865@naver.com>
2025-07-15 18:22:25 +0900

    renderElement 구현
    - vNode 표준화 이후 container에 vNode 추가
    - 최초 렌더링 시점에만 이벤트 등록

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,10 +1,18 @@
 import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
-import { updateElement } from "./updateElement";
+
+let isInitialized = false;
 
 export function renderElement(vNode, container) {
-  // 최초 렌더링시에는 createElement로 DOM을 생성하고
-  // 이후에는 updateElement로 기존 DOM을 업데이트한다.
-  // 렌더링이 완료되면 container에 이벤트를 등록한다.
+  const normalizedVNode = normalizeVNode(vNode);
+
+  const el = createElement(normalizedVNode);
+  container.innerHTML = "";
+  container.appendChild(el);
+
+  if (!isInitialized) {
+    setupEventListeners(container);
+    isInitialized = true;
+  }
 }
