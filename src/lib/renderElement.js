287608dc4a2bce5acdcddb2d53496680f23103d6
BangDori <qudwns8865@naver.com>
2025-07-15 20:19:08 +0900

    renderElement 메서드 이벤트 수정
    - 이벤트 처리 제거 로직을 setupEventListeners에 추가하여 동적 이벤트 허용하도록 반영

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -2,8 +2,12 @@ import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
 
-let isInitialized = false;
-
+/**
+ * 가상 노드를 실제 요소로 변환하여 렌더링하는 함수
+ *
+ * @param {any} vNode 가상 노드
+ * @param {HTMLElement} container 렌더링할 컨테이너
+ */
 export function renderElement(vNode, container) {
   const normalizedVNode = normalizeVNode(vNode);
 
@@ -11,8 +15,5 @@ export function renderElement(vNode, container) {
   container.innerHTML = "";
   container.appendChild(el);
 
-  if (!isInitialized) {
-    setupEventListeners(container);
-    isInitialized = true;
-  }
+  setupEventListeners(container);
 }
