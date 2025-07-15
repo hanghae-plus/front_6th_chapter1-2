42249af1fd10a194b15eb131ea0b1c33caf8fc86
이의찬 <uichan@MacBook-Air-3.local>
2025-07-15 21:44:58 +0900

    feat: createVNode 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,9 @@
+console.log(createVNode("div", { id: "test" }, "Hello"));
+
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props,
+    children: [...children].flat(Infinity).filter((child) => child !== null && child !== undefined && child !== false),
+  };
 }
