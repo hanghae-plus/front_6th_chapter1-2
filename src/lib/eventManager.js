9051109ca582565eed8b1baec37e23293dd98767
hmyo2853 <hmyo2853@gmail.com>
2025-07-15 19:28:02 +0900

    Feature : eventManager에 setupEventListeners, addEvent 함수 로직 추가

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,32 @@
-export function setupEventListeners(root) {}
+// 객체별 이벤트 저장소
+const eventRegistry = new Map();
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  // 클릭 이벤트 위임
+  root.addEventListener("click", (e) => {
+    eventRegistry.forEach((eventMap, element) => {
+      if (element === e.target) {
+        eventMap.forEach((handler) => {
+          // 이벤트 실행시 event를 전달
+          // e.preventDefault나 e.stopPropagation을 통해 이벤트 전파를 막는 용도
+          handler(e);
+        });
+      }
+    });
+  });
+}
 
-export function removeEvent(element, eventType, handler) {}
+export function addEvent(element, eventType, handler) {
+  // 모든 이벤트는 이벤트 위임 방식으로 setupEventListeners에서 처리해야함
+  // 해당 이벤트가 없다면 생성
+  if (!eventRegistry.has(element)) {
+    eventRegistry.set(element, new Map());
+  }
+
+  const eventMap = eventRegistry.get(element);
+  eventMap.set(eventType, handler);
+
+  return element;
+}
+
+// export function removeEvent(element, eventType, handler) {}
