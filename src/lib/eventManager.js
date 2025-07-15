05eedcbb83b0bc3b2857fa909b07d51f560e4d47
JangRuBin2 <wkdfnqls2465@gmail.com>
2025-07-16 00:29:06 +0900

    feat: addEvent구현, WeakMap이용하여 이벤트 등록

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,37 @@
-export function setupEventListeners(root) {}
+//가비지 컬렉션 되면 자동제거
+const eventStore = new WeakMap();
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  root.addEventListener("click", (e) => {
+    let target = e.target;
+    // console.log("target:", target);
+    if (target) {
+      const handlers = eventStore.get(target);
+      // console.log(handlers);
+      if (handlers && handlers["click"]) {
+        handlers["click"].forEach((fn) => fn(e));
+      }
+      // console.log(e.cancelBubble);
+      if (e.cancelBubble) return; // stopPropagation
+      target = target.parentNode;
+    }
+  });
+}
 
-export function removeEvent(element, eventType, handler) {}
+export function addEvent(element, eventType, handler) {
+  if (!eventStore.has(element)) {
+    eventStore.set(element, {});
+  }
+  const handlers = eventStore.get(element);
+
+  if (!handlers[eventType]) {
+    handlers[eventType] = new Set();
+  }
+
+  handlers[eventType].add(handler);
+  console.log(handlers);
+}
+
+export function removeEvent(element, eventType, handler) {
+  console.log(element, handler, eventType);
+}
