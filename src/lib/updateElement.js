69796e6b82c8416dcfb4a8afc2882a9555b026c0
Hwirin-Kim <kimhuirin@gmail.com>
2025-07-16 18:32:13 +0900

    fix : updateElement 변경 - children에 직접 할당하는 방식 제거 (getter-only 에러 수정) - 무작정 이벤트를 지우고 할당하지 않고 기존과 다를때만 변경하도록 최적화

--- src/lib/updateElement.js
+++ src/lib/updateElement.js
@@ -1,6 +1,8 @@
 import { addEvent, removeEvent } from "./eventManager";
 import { createElement } from "./createElement.js";
 
+const BOOLEAN_PROPS = ["checked", "disabled", "selected", "readOnly"];
+
 function updateAttributes(dom, newProps = {}, oldProps = {}) {
   // 새 props 추가 또는 변경
   for (const key in newProps) {
@@ -10,20 +12,17 @@ function updateAttributes(dom, newProps = {}, oldProps = {}) {
     if (newVal !== oldVal) {
       if (key.startsWith("on")) {
         const eventType = key.slice(2).toLowerCase();
-
-        // 기존 핸들러 제거
-        if (typeof oldVal === "function") {
-          removeEvent(dom, eventType, oldVal);
-        }
-
-        // 새 핸들러 등록
-        if (typeof newVal === "function") {
-          addEvent(dom, eventType, newVal);
-        }
+        if (typeof oldVal === "function") removeEvent(dom, eventType, oldVal);
+        if (typeof newVal === "function") addEvent(dom, eventType, newVal);
       } else if (key === "className") {
         dom.setAttribute("class", newVal);
-      } else {
+      } else if (BOOLEAN_PROPS.includes(key)) {
+        dom[key] = Boolean(newVal);
+      } else if (key in dom && key !== "children") {
+        // children은 getter-only
         dom[key] = newVal;
+      } else {
+        dom.setAttribute(key, newVal);
       }
     }
   }
@@ -36,116 +35,62 @@ function updateAttributes(dom, newProps = {}, oldProps = {}) {
         removeEvent(dom, eventType, oldProps[key]);
       } else if (key === "className") {
         dom.removeAttribute("class");
+      } else if (BOOLEAN_PROPS.includes(key)) {
+        dom[key] = false;
       } else {
-        dom[key] = undefined;
+        dom.removeAttribute(key);
       }
     }
   }
 }
-
-export function updateElement(parentElement, newNode, oldNode, index = 0) {
-  const existingDom = parentElement.childNodes[index];
+export function updateElement(parent, newNode, oldNode, index = 0) {
+  const dom = parent.childNodes[index];
 
   // 1. oldNode가 없으면 새로 추가
   if (!oldNode) {
-    parentElement.appendChild(createElement(newNode));
+    parent.appendChild(createElement(newNode));
     return;
   }
 
-  // 2. newNode가 없으면 기존 DOM 제거
+  // 2. newNode가 없으면 제거
   if (!newNode) {
-    if (existingDom) parentElement.removeChild(existingDom);
+    if (dom) parent.removeChild(dom);
     return;
   }
 
-  // 3. 타입이 다르면 교체
+  // 3. 타입 또는 태그가 다르면 교체
   if (
     typeof newNode !== typeof oldNode ||
     (typeof newNode === "string" && newNode !== oldNode) ||
     (newNode.type && newNode.type !== oldNode.type)
   ) {
-    parentElement.replaceChild(createElement(newNode), existingDom);
+    parent.replaceChild(createElement(newNode), dom);
     return;
   }
 
-  // 4. 텍스트 노드면 내용만 업데이트
+  // 4. 텍스트 노드 업데이트
   if (typeof newNode === "string") {
-    if (existingDom.textContent !== newNode) {
-      existingDom.textContent = newNode;
+    if (dom.textContent !== newNode) {
+      dom.textContent = newNode;
     }
     return;
   }
 
-  // 5. props 업데이트
-  updateAttributes(existingDom, newNode.props || {}, oldNode.props || {});
+  // 5. 속성 업데이트
+  updateAttributes(dom, newNode.props || {}, oldNode.props || {});
 
-  // 6. 자식 노드 재귀적으로 diff
+  // 6. 자식 노드 비교
   const newChildren = newNode.children || [];
   const oldChildren = oldNode.children || [];
+
+  // 7. 초과된 자식 제거
+  while (dom.childNodes.length > newChildren.length) {
+    dom.removeChild(dom.lastChild);
+  }
+
+  // 8. 나머지 재귀적 업데이트
   const max = Math.max(newChildren.length, oldChildren.length);
-
-  // 7. oldChildren이 더 많으면 초과된 노드 제거
-  if (oldChildren.length > newChildren.length) {
-    for (let i = newChildren.length; i < oldChildren.length; i++) {
-      const childDom = existingDom.childNodes[i];
-      if (childDom) {
-        existingDom.removeChild(childDom);
-      }
-    }
-  }
-
   for (let i = 0; i < max; i++) {
-    updateElement(existingDom, newChildren[i], oldChildren[i], i);
-  }
-
-  // 초과된 자식 DOM 제거
-  while (existingDom.childNodes.length > newChildren.length) {
-    existingDom.removeChild(existingDom.lastChild);
+    updateElement(dom, newChildren[i], oldChildren[i], i);
   }
 }
-
-// export function updateElement(parentElement, newNode, oldNode, index = 0) {
-//   const existingDom = parentElement.childNodes[index];
-
-//   // 1. oldNode가 없으면 새로 추가
-//   if (!oldNode) {
-//     parentElement.appendChild(createElement(newNode));
-//     return;
-//   }
-
-//   // 2. newNode가 없으면 기존 DOM 제거
-//   if (!newNode) {
-//     if (existingDom) parentElement.removeChild(existingDom);
-//     return;
-//   }
-
-//   // 3. 타입(태그/텍스트)이 다르면 교체
-//   if (
-//     typeof newNode !== typeof oldNode ||
-//     (typeof newNode === "string" && newNode !== oldNode) ||
-//     (newNode.type && newNode.type !== oldNode.type)
-//   ) {
-//     parentElement.replaceChild(createElement(newNode), existingDom);
-//     return;
-//   }
-
-//   // 4. 텍스트 노드면 내용만 업데이트
-//   if (typeof newNode === "string") {
-//     if (existingDom.textContent !== newNode) {
-//       existingDom.textContent = newNode;
-//     }
-//     return;
-//   }
-
-//   // 5. props 업데이트
-//   updateProps(existingDom, newNode.props, oldNode.props);
-
-//   // 6. 자식 노드 재귀적으로 diff
-//   const newChildren = newNode.children || [];
-//   const oldChildren = oldNode.children || [];
-//   const max = Math.max(newChildren.length, oldChildren.length);
-
-//   for (let i = 0; i < max; i++) {
-//     updateElement(existingDom, newChildren[i], oldChildren[i], i);
-//   }
-// }
