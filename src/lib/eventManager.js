35fb00433f5c7511c711c02d6d5ede0b54f65c74
JangRuBin2 <wkdfnqls2465@gmail.com>
2025-07-17 00:47:29 +0900

    refactor: 이벤트 스토어 수정, Map 사용법에 맞춰 has, get문법 사용...전에는 왜 동작한거지?

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -2,10 +2,9 @@
 const eventStore = new Map();
 
 export function setupEventListeners(root) {
-  for (const event in eventStore) {
+  for (const [event, handlers] of eventStore.entries()) {
     root.addEventListener(event, (e) => {
-      // console.log(event);
-      eventStore[event].forEach(({ element, handler }) => {
+      handlers.forEach(({ element, handler }) => {
         if (element.contains(e.target)) handler(e);
       });
     });
@@ -13,16 +12,24 @@ export function setupEventListeners(root) {
 }
 
 export function addEvent(element, eventType, handler) {
-  if (!eventStore.has(eventType)) eventStore[eventType] = new Set();
-  eventStore[eventType].add({ element, handler });
+  if (!eventStore.has(eventType)) {
+    eventStore.set(eventType, new Set());
+  }
+  eventStore.get(eventType).add({ element, handler });
 }
 
 export function removeEvent(element, eventType, handler) {
-  if (eventStore[eventType]) {
-    eventStore[eventType].forEach((item) => {
-      if (item.element === element && item.handler === handler) {
-        eventStore[eventType].delete(item);
-      }
-    });
+  const handlers = eventStore.get(eventType);
+  if (!handlers) return;
+
+  for (const item of handlers) {
+    if (item.element === element && item.handler === handler) {
+      handlers.delete(item);
+    }
+  }
+
+  //메모리 정리
+  if (handlers.size === 0) {
+    eventStore.delete(eventType);
   }
 }
