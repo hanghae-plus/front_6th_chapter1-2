148add2e8204af8676a29a2b1d49cc972c8d5887
JiHoon-0330 <ljihoon0330@gmail.com>
2025-07-16 14:51:11 +0900

    feat: updateElement 함수 구현
    Refs: #9

--- src/lib/updateElement.js
+++ src/lib/updateElement.js
@@ -1,6 +1,64 @@
-import { addEvent, removeEvent } from "./eventManager";
-import { createElement } from "./createElement.js";
+import { isNullish, isString } from "../utils/typeCheck";
+import { removeAttribute, updateAttribute } from "../utils/updateAttribute";
+import { createElement } from "./createElement";
 
-function updateAttributes(target, originNewProps, originOldProps) {}
+export function updateElement(parentElement, newNode, oldNode, index = 0) {
+  const currentDomNode = parentElement.childNodes[index];
 
-export function updateElement(parentElement, newNode, oldNode, index = 0) {}
+  if (newNode && isNullish(oldNode)) {
+    parentElement.appendChild(createElement(newNode));
+    return;
+  }
+
+  if (isNullish(newNode) && oldNode) {
+    parentElement.removeChild(currentDomNode);
+    return;
+  }
+
+  if (isString(newNode) && isString(oldNode)) {
+    if (newNode !== oldNode) {
+      currentDomNode.nodeValue = newNode;
+    }
+    return;
+  }
+
+  if (newNode.type !== oldNode.type) {
+    parentElement.replaceChild(createElement(newNode), currentDomNode);
+    return;
+  }
+
+  updateProps(currentDomNode, newNode, oldNode);
+
+  let i = 0;
+  let maxChildrenLength = Math.max(newNode.children.length, oldNode.children.length);
+  while (i < maxChildrenLength) {
+    const newChild = newNode.children[i];
+    const oldChild = oldNode.children[i];
+    if (!newChild && oldChild) {
+      updateElement(currentDomNode, null, oldChild, i);
+      maxChildrenLength -= 1;
+    } else {
+      updateElement(currentDomNode, newChild, oldChild, i);
+      i += 1;
+    }
+  }
+}
+
+function updateProps(target, newNode, oldNode) {
+  const { props: newProps = {} } = newNode;
+  const { props: oldProps = {} } = oldNode;
+  const newPropsKeys = Object.keys(newProps);
+  const oldPropsKeys = Object.keys(oldProps);
+  const allKeys = new Set([...newPropsKeys, ...oldPropsKeys]);
+
+  for (const key of allKeys) {
+    if (!(key in newProps)) {
+      removeAttribute(target, key, oldProps[key]);
+      continue;
+    }
+
+    if (newProps[key] !== oldProps[key]) {
+      updateAttribute(target, key, newProps[key]);
+    }
+  }
+}
