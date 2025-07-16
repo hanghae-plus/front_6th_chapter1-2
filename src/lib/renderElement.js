3c55662029e5933023f4bdafec0b5694bc0812ae
JiHoon-0330 <ljihoon0330@gmail.com>
2025-07-16 14:54:23 +0900

    feat: renderElement 함수에 업데이트 추가

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,16 +1,19 @@
-import { cleanupEventListeners, setupEventListeners } from "./eventManager";
+import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
-// import { updateElement } from "./updateElement";
+import { updateElement } from "./updateElement";
+
+let normalizedVNode = null;
 
 export function renderElement(vNode, container) {
-  // 최초 렌더링시에는 createElement로 DOM을 생성하고
-  // 이후에는 updateElement로 기존 DOM을 업데이트한다.
-  // 렌더링이 완료되면 container에 이벤트를 등록한다.
+  const prevVNode = normalizedVNode;
+  normalizedVNode = normalizeVNode(vNode);
 
-  cleanupEventListeners();
-  const $el = createElement(normalizeVNode(vNode));
-  container.innerHTML = "";
-  container.appendChild($el);
-  setupEventListeners(container);
+  if (container.hasChildNodes() && prevVNode) {
+    updateElement(container, normalizedVNode, prevVNode);
+  } else {
+    const $el = createElement(normalizedVNode);
+    container.appendChild($el);
+    setupEventListeners(container);
+  }
 }
