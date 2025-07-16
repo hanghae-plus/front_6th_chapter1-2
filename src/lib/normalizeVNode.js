b3604a6e23b53b2244137063a26c5a902e865232
nimusmix <nimusmix@gmail.com>
2025-07-17 00:48:14 +0900

    refactor: vNode.type이 function일 때 normalizedChildren 추가

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -14,7 +14,7 @@ export function normalizeVNode(vNode) {
   }
 
   if (isFunction(vNode.type)) {
-    const props = { ...(vNode.props ?? {}), children: vNode.children };
+    const props = { ...(vNode.props ?? {}), children: normalizeChildren(vNode.children) };
     return normalizeVNode(vNode.type(props));
   }
 
