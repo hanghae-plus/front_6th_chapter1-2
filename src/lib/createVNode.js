347704f6722b793550104660b8f51eb153a3d9b4
이의찬 <uichan@Seossi.localdomain>
2025-07-17 15:38:28 +0900

    docs: createVNode 주석 추가

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,5 +1,10 @@
-console.log(createVNode("div", { id: "test" }, "Hello"));
-
+/**
+ * VirtualNode를 생성하는 함수
+ * @param {string} type - 노드의 타입
+ * @param {Object} props - 노드의 속성
+ * @param {...any} children - 노드의 자식 노드
+ * @returns {Object} 가상돔 노드
+ */
 export function createVNode(type, props, ...children) {
   return {
     type,
