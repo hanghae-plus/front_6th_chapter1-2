038ca38d0d2ed67ce2406de1bd0f1724d840e57f
이유진 <elli.stdev@gmail.com>
2025-07-18 06:08:55 +0900

    ci: deploy test

--- vite.config.js
+++ vite.config.js
@@ -17,9 +17,15 @@ export default mergeConfig(
     base: process.env.NODE_ENV === "production" ? "/front_6th_chapter1-2/" : "/",
 
     build: {
+      outDir: "dist",
+      assetsDir: "assets",
+      sourcemap: false,
       rollupOptions: {
         input: {
           main: resolve(__dirname, "index.html"),
+        },
+        output: {
+          manualChunks: undefined,
         },
       },
     },
