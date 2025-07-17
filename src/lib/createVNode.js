5703bdb1a4cbd9b29e7831acb33e3c2065c82074
Jaehwan Choi <lieblichoi@gmail.com>
2025-07-17 19:47:54 +0900

    Feature: createVNode 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,8 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  const flatChildren = children.flat(Infinity);
+  const filteredChildren = flatChildren.filter(
+    (child) => child !== null && child !== undefined && typeof child !== "boolean",
+  );
+
+  return { type, props, children: filteredChildren };
 }
