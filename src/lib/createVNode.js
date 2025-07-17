38d7ed6967bb564a7056f22713c787b00befd706
YangS1s <sungjinahim@gmail.com>
2025-07-17 14:16:59 +0900

    feat: build commit

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -2,7 +2,6 @@
 export function createVNode(type, props, ...children) {
   // 자바스크립트에서 0은 falsy값이라 테스트코드에서는 출력이 안될수 있으므로
   // 예외처리를 해줘야할거 같음
-  // console.log("type => ", type);
 
   return {
     type,
