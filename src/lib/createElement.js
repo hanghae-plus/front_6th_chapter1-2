0b6778c875ee7334c7b1546ae9fb71b902494ba5
nimusmix <nimusmix@gmail.com>
2025-07-18 02:04:27 +0900

    fix: createElement Boolean Props 처리

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -22,14 +22,25 @@ export function createElement(vNode) {
   return $el;
 }
 
+const BOOLEAN_PROPS = new Set(["checked", "disabled", "selected", "readOnly"]);
+
 function updateAttributes($el, props) {
   for (const [key, value] of Object.entries(props)) {
     if (key === "className") {
       $el.setAttribute("class", value);
-    } else if (key.startsWith("on")) {
-      addEvent($el, key.slice(2).toLowerCase(), value);
-    } else {
-      $el.setAttribute(key, value);
+      continue;
     }
+
+    if (key.startsWith("on")) {
+      addEvent($el, key.slice(2).toLowerCase(), value);
+      continue;
+    }
+
+    if (BOOLEAN_PROPS.has(key)) {
+      $el[key] = !!value;
+      continue;
+    }
+
+    $el.setAttribute(key, value);
   }
 }
