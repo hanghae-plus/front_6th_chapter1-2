107bd39d0e2c906dad500f719b5cb2c1d3b17938
JOYOUNGMIN <jym0421@ciel.co.kr>
2025-07-17 19:04:02 +0900

    feat: createElement

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,36 @@
-import { addEvent } from "./eventManager";
+// import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
 
-function updateAttributes($el, props) {}
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(String(vNode));
+  }
+
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+
+    vNode.forEach((node) => fragment.appendChild(createElement(node)));
+
+    return fragment;
+  }
+
+  const el = document.createElement(vNode.type);
+  updateAttributes(el, vNode.props ?? {});
+
+  el.append(...vNode.children.map(createElement));
+
+  return el;
+}
+
+function updateAttributes($el, props) {
+  Object.entries(props).forEach(([key, value]) => {
+    if (key === "className") {
+      $el.setAttribute("class", value);
+    } else {
+      $el.setAttribute(key, value);
+    }
+  });
+}
