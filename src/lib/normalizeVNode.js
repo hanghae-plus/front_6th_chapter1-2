e9685bb2812b8ec046cd66736820c69ed0602332
Angie Lee <angielxx94@gmail.com>
2025-07-17 12:25:43 +0900

    feat: normalize 함수 수정

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -17,29 +17,18 @@ export function normalizeVNode(vNode) {
     return vNode.map(normalizeVNode).filter((child) => child !== "");
   }
 
-  // 컴포넌트인 경우
-  if (typeof vNode === "object" && typeof vNode.type === "function") {
-    const Component = vNode.type;
-    const props = vNode.props ?? {};
-
-    // children을 props에 포함하여 컴포넌트에 전달
+  if (typeof vNode.type === "function") {
+    const componentFunction = vNode.type;
     const propsWithChildren = {
-      ...props,
-      children: vNode.children
-        .map(normalizeVNode)
-        .filter((child) => child !== ""),
+      ...vNode.props,
+      children: vNode.children,
     };
 
-    const componentVNode = Component(propsWithChildren);
-
-    if (!componentVNode) return "";
-
-    // 컴포넌트가 반환한 vNode를 재귀적으로 정규화
-    return normalizeVNode(componentVNode);
+    return normalizeVNode(componentFunction(propsWithChildren));
   }
 
   return {
     ...vNode,
-    children: vNode.children.map(normalizeVNode),
+    children: vNode.children.map(normalizeVNode).filter(Boolean),
   };
 }
