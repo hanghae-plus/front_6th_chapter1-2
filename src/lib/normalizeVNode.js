cfb9152f2c23efec24656c8b93a99084eda308d0
bebusl <bebus1998@naver.com>
2025-07-17 17:52:04 +0900

    feat: normalizeVNode 작성

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,19 @@
 export function normalizeVNode(vNode) {
+  if (typeof vNode === "boolean" || vNode === undefined || vNode === null) {
+    return "";
+  }
+  if (typeof vNode === "string" || typeof vNode === "number") return `${vNode}`;
+
+  if (typeof vNode.type === "function") {
+    const mergedProps = { ...(vNode.props || {}), children: vNode.children };
+    return normalizeVNode(vNode.type(mergedProps));
+  }
+
+  // 자식들도 normalize
+  if (Array.isArray(vNode.children)) {
+    const normalizedChildren = vNode.children.map((child) => normalizeVNode(child)).filter((value) => value !== "");
+    vNode.children = normalizedChildren;
+  }
+
   return vNode;
 }
