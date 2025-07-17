ede175644657fd32da23e8d7e25bf3821b44413e
bebusl <bebus1998@naver.com>
2025-07-18 05:41:54 +0900

    feat: createElement 작성

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,40 @@
-import { addEvent } from "./eventManager";
+// import { addEvent } from "./eventManager";
+import { normalizeVNode } from "./normalizeVNode";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
 
-function updateAttributes($el, props) {}
+    vNode.forEach((v) => {
+      const element = document.createElement(v.type);
+      v.children.forEach((child) => element.appendChild(createElement(child)));
+      fragment.appendChild(element);
+    });
+    return fragment;
+  }
+  const normalizedVNode = normalizeVNode(vNode);
+
+  if (typeof normalizedVNode === "string") return document.createTextNode(normalizedVNode);
+
+  if (typeof vNode.type === "function") throw Error;
+
+  const element = document.createElement(vNode.type);
+
+  if (vNode.props) updateAttributes(element, vNode.props);
+
+  vNode.children.forEach((child) => {
+    element.appendChild(createElement(child));
+  });
+
+  return element;
+}
+
+function updateAttributes($el, props) {
+  for (const [key, value] of Object.entries(props || {})) {
+    let normalizedKey = key === "className" ? "class" : key;
+
+    if (value !== undefined && value !== null) {
+      $el.setAttribute(normalizedKey, value);
+    }
+  }
+}
